558c3ea9ad5f238239ef5982487292ba51fc7c4e
Ashtamcode <70945937+Ashtamcode@users.noreply.github.com>
2024-09-02 19:16:09 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import pandas as pd
 Acess_code = 123
 std_code = 567
 ac_check = int(input("Please Enter Acess code"))
-
+df = None
 if ac_check == Acess_code:
     print("Welcome to Admin")
     choice = int(input("Please enter your Choice \n 1 for Adding a student \n 2 for Updating a Student data \n 3 for deleting a student Data \n 4 for searching a Student User_ID \n 5 For creating an Exam \n" ))
@@ -20,14 +20,18 @@ if ac_check == Acess_code:
     else:
         choice = input("Enter the subject of which the exam you want to create \n for Maths type M \n for Physics type P \n for Chemistry type C\n")
         df = e.create_exam(choice)
+        df.to_csv('exam_data.csv', index=False)
 elif ac_check == std_code:
     
     print("Welcome to Student")
-    df = e.create_exam()
     choice1 = int(input("Enter 1 to take your saved exam or 2 to exit the interface"))
     if choice1 == 1:
-        print(df)
-        e.take_exam(df)
+
+        df = pd.read_csv('exam_data.csv')
+        dff = pd.DataFrame(df)
+        #print(dff)
+        e.take_exam(dff)
+
     else:
         pass
 else:
